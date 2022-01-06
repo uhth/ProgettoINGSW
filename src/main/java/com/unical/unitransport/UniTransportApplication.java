0406158173d9089412b8ecd62e8adba4c35dd194
uhth <vincenzorizzomy@gmail.com>
2022-01-06 15:39:30 +0100

    added dbmanager

--- src/main/java/com/unical/unitransport/UniTransportApplication.java
+++ src/main/java/com/unical/unitransport/UniTransportApplication.java
@@ -3,11 +3,14 @@ package com.unical.unitransport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.unical.unitransport.controller.DatabaseManager;
+
 @SpringBootApplication
 public class UniTransportApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UniTransportApplication.class, args);
+		DatabaseManager.getConnection();
 	}
 
 }
