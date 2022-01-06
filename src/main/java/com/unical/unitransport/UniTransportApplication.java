0f3d3fd599854265da80639c68664e2104e25431
uhth <vincenzorizzomy@gmail.com>
2022-01-06 15:27:51 +0100

    project init

---
+++ src/main/java/com/unical/unitransport/UniTransportApplication.java
@@ -0,0 +1,13 @@
+package com.unical.unitransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniTransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniTransportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unical/unitransport/UniTransportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unical.unitransport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniTransportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
