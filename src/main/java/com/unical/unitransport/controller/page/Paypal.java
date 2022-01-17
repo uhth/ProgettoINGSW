fa5a05e94566c724e48beac29318d885a3736108
PIERPY <PIERPY@DESKTOP-9DU34VM>
2022-01-17 19:11:52 +0100

    modifiche del mio branch portate sul dev

---
+++ src/main/java/com/unical/unitransport/controller/page/Paypal.java
@@ -0,0 +1,13 @@
+package com.unical.unitransport.controller.page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Paypal {
+
+	@GetMapping("paypal")
+	public String payment() {
+		return "paypal";
+	}
+}
