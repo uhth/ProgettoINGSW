5284478d761f2639c7522c41b8efc19396ac3f5c
uhth <vincenzorizzomy@gmail.com>
2022-01-17 15:29:37 +0100

    simple rename

--- src/main/java/com/unical/unitransport/controller/page/IndexPageController.java
+++ src/main/java/com/unical/unitransport/controller/page/IndexPageController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class IndexPageController {
 	
 	@GetMapping("/")
 	@RequestMapping
