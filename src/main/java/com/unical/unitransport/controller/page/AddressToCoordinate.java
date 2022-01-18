1d62de4211ae1c25ce180a6a57978c81bf842fc6
Luca24 <Luca24@Luca>
2022-01-18 12:59:13 +0100

    First step Address To Coordinate

---
+++ src/main/java/com/unical/unitransport/controller/page/AddressToCoordinate.java
@@ -0,0 +1,78 @@
+package com.unical.unitransport.controller.page;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Pattern;
+
+public class AddressToCoordinate {
+
+	private String address;
+	private String URL = "http://api.positionstack.com/v1/forward?access_key=3a0f0a316dd93089d6bdfbf66e4a95d4&query=";
+
+	public AddressToCoordinate(String address) {
+		super();
+		this.address = address;
+	}
+
+	public String getLatitude() {
+		String theUrl = URL + address;
+		StringBuilder content = new StringBuilder();
+		try {
+			URL url = new URL(theUrl);
+			URLConnection urlConnection = url.openConnection();
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				content.append(line + "\n");
+			}
+			bufferedReader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String c = content.toString();
+		String[] parts = c.split(Pattern.quote(":"));
+
+		String lat = parts[2];
+		String lng = parts[3];
+		String[] splitlat = lat.split(Pattern.quote(","));
+		String[] splitlong = lng.split(Pattern.quote(","));
+
+		String finalLat = splitlat[0];
+		String finalLong = splitlong[0];
+
+		return finalLat;
+	}
+
+	public String getLongitude() {
+
+		String theUrl = URL + address;
+		StringBuilder content = new StringBuilder();
+		try {
+			URL url = new URL(theUrl);
+			URLConnection urlConnection = url.openConnection();
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				content.append(line + "\n");
+			}
+			bufferedReader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String c = content.toString();
+		String[] parts = c.split(Pattern.quote(":"));
+
+		String lat = parts[2];
+		String lng = parts[3];
+		String[] splitlat = lat.split(Pattern.quote(","));
+		String[] splitlong = lng.split(Pattern.quote(","));
+
+		String finalLat = splitlat[0];
+		String finalLong = splitlong[0];
+
+		return finalLong;
+	}
+
+}
--- src/main/java/com/unical/unitransport/controller/page/TrackingPageController.java
+++ src/main/java/com/unical/unitransport/controller/page/TrackingPageController.java
@@ -1,13 +1,17 @@
 package com.unical.unitransport.controller.page;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,14 +20,12 @@ import com.unical.unitransport.controller.persistence.shipment.ShipmentsDAO;
 
 @Controller
 public class TrackingPageController {
-	
 
 	@PostMapping("/trackingService")
-	public String check( HttpServletRequest req,
-						 HttpServletResponse res, 
-						 @RequestParam( value = "tracking_box", required = true ) String tracking ) throws IOException {
+	public String check(HttpServletRequest req, HttpServletResponse res,
+			@RequestParam(value = "tracking_box", required = true) String tracking) throws IOException {
 
-		Shipment spedizione = ShipmentsDAO.getByTrackingNumber( tracking );
+		Shipment spedizione = ShipmentsDAO.getByTrackingNumber(tracking);
 
 		System.out.println("NTracking: " + tracking);
 		System.out.println(spedizione);
@@ -37,13 +39,20 @@ public class TrackingPageController {
 		String posCorriere = spedizione.getLastLocation();
 		String posDestinatario = spedizione.getLastLocation();
 		String posMittente = spedizione.getLastLocation();
-
-
-		// draw on js
-
+		System.out.println(posCorriere);
+		
+		
+		AddressToCoordinate indirizzo = new AddressToCoordinate(posCorriere);
+		System.out.println(indirizzo.getLatitude());
+		System.out.println(indirizzo.getLongitude());
+				
+				
+	
 
 		return "tracking_gmapsPositivo";
 	}
 
+	
 
 }
+
