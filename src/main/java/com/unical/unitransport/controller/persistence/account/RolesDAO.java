0202aff334e7d39846b71eaaa819a72bf5951f58
Luca24 <Luca24@Luca>
2022-01-22 12:56:53 +0100

    added roles on create table role

--- src/main/java/com/unical/unitransport/controller/persistence/account/RolesDAO.java
+++ src/main/java/com/unical/unitransport/controller/persistence/account/RolesDAO.java
@@ -30,6 +30,9 @@ public class RolesDAO {
 					+ "role_name VARCHAR ( 255 ) UNIQUE NOT NULL );";
 			statement.executeUpdate( sql );	
 			statement.close();
+			insert(new Role("user"));
+			insert(new Role("corriere"));
+			insert(new Role("admin"));
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
