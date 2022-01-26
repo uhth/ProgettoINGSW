118a0e8e1eb9f13dd12dfdf7a69c4e4985979f27
PIERPY <PIERPY@DESKTOP-9DU34VM>
2022-01-26 21:33:53 +0100

    collegata pagina pagamento completato alla generazione del codice

--- src/main/resources/static/js/prenota_ritiro.js
+++ src/main/resources/static/js/prenota_ritiro.js
@@ -1,5 +1,8 @@
 function prosegui() {
 	if (document.getElementById("Online").checked == true){
+		sessionStorage.setItem("luogoRitiro", document.getElementById("ViaRitiro").value);
+		sessionStorage.setItem("luogoConsegna", document.getElementById("ViaConsegna").value);
+		sessionStorage.setItem("emailDestinatario", document.getElementById("EmailDest").value);	
   		window.location.href = "/payment";
 	}
  	else if(document.getElementById("Contrassegno").checked == true){
